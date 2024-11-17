a955d344714150c7f26c283702f28482bb6e5519
IanDob26 <ipd5080@psu.edu>
2024-11-17 17:53:55 -0500

    first commit

---
+++ project-1.js
@@ -0,0 +1,140 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import { siteCard } from "./site-card";
+
+let items = [];
+let title = "";
+let siteURL = "";
+
+export class project1 extends DDDSuper(I18NMixin(LitElement)) {
+  static get tag() {
+    return "project-1";
+  }
+
+  constructor() {
+    super();
+  }
+
+  // Lit reactive properties
+  static get properties() {
+    return {};
+  }
+
+  // Lit scoped styles
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        #input {
+          width: 50%;
+          font-size: 24px;
+          padding: var(--ddd-spacing-4);
+        }
+        #analyze {
+          font-size: 24px;
+          padding: var(--ddd-spacing-4);
+          margin-left: var(--ddd-spacing-2);
+        }
+        #controls {
+          display: flex;
+          justify-content: center;
+          margin: var(--ddd-spacing-4);
+        }
+
+        #results {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        #title {
+          text-align: center;
+        }
+        site-card {
+          width: 20%;
+          min-width: 300px;
+          margin: var(--ddd-spacing-10);
+          border: 1px solid white;
+          position: relative;
+          padding-bottom: 232px;
+        }
+      `,
+    ];
+  }
+
+  // Lit render the HTML
+  render() {
+    return html`
+      <div id="controls">
+        <input id="input" placeholder="Site address here" />
+        <button id="analyze" @click=${this.analyzeSite}>Analyze</button>
+      </div>
+      <div id="title"></div>
+      <div id="results"></div>
+    `;
+  }
+
+  dateToString(timestamp) {
+    const date = new Date(timestamp * 1000);
+    return date.toUTCString();
+  }
+
+  checkSite() {
+    const input = this.shadowRoot.getElementById("input");
+    let site = input.value;
+    if (!input.value.includes("site.json")) {
+      site = input.value + "site.json";
+    }
+    siteURL = site.replace("site.json", "");
+    return site;
+  }
+
+  async analyzeSite() {
+    // Get the JSON data
+    const site = this.checkSite();
+    const response = await fetch(site);
+    const data = await response.json();
+
+    // Set the title and items
+    title = data.title;
+    items = data.items;
+
+    // get container
+    const container = this.shadowRoot.getElementById("results");
+    // clear container
+    container.innerHTML = "";
+    // get title element
+    const titleElement = this.shadowRoot.getElementById("title");
+    //set title
+    titleElement.innerHTML = data.title;
+
+    for (let i = 0; i < items.length; i++) {
+      const card = document.createElement("site-card");
+      card.name = items[i].title;
+      card.description = items[i].description;
+      card.createdTime = this.dateToString(items[i].metadata.created);
+      card.updatedTime = this.dateToString(items[i].metadata.updated);
+
+      if (items[i].metadata.images.length == 0) {
+        card.logo =
+          "https://iam.hax.psu.edu/ipd5080/sites/hwist256week2/assets/banner.jpg"; // change
+      } else {
+        card.logo = siteURL + items[i].metadata.images[0];
+      }
+      console.log(card.logo);
+
+      container.appendChild(card);
+    }
+
+    console.log(items);
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(project1.tag, project1);
---
+++ site-card.js
@@ -0,0 +1,106 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+
+export class siteCard extends DDDSuper(I18NMixin(LitElement)) {
+  static get tag() {
+    return "site-card";
+  }
+
+  constructor() {
+    super();
+    this.name = "";
+    this.description = "";
+    this.logo ="";
+    this.createdTime = "";
+    this.updatedTime = "";
+  }
+
+  // Lit reactive properties
+  static get properties() {
+    return {
+      name: { type: String },
+      description: { type: String },
+      logo: { type: String },
+      createdTime: { type: String },
+      updatedTime: { type: String },
+    };
+  }
+
+  // Lit scoped styles
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        .card {
+          display: flex;
+          flex-direction: column;
+          text-align: center;
+        }
+        .image {
+          width: 75%;
+          max-height: 200px;
+          margin: var(--ddd-spacing-12) auto;
+        }
+        .logo {
+          width: 100%;
+          border-radius: var(--ddd-radius-md);
+        }
+        .dates {
+          font-size: 12px;
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          position: absolute;
+          bottom: 0;
+        }
+        .info {
+          position: absolute;
+          bottom: var(--ddd-spacing-16);
+        }
+        .name {
+          margin: 0 var(--ddd-spacing-8);
+        }
+        .changed {
+          margin: 0 var(--ddd-spacing-4);
+          font-style: italic;
+          text-align: center;
+        }
+        .description {
+          font-size: 16px;
+          margin: var(--ddd-spacing-2) var(--ddd-spacing-4);
+        }
+      `,
+    ];
+  }
+
+  // Lit render the HTML
+  render() {
+    return html`
+      <div class="card">
+        <div class="image">
+          <img class="logo" src="${this.logo}" alt="${this.name}" />
+        </div>
+        <div class="info">
+          <h2 class="name">${this.name}</h2>
+          <p class="description">${this.description}</p>
+        </div>
+
+        <div class="dates">
+          <div class="changed">Created ${this.createdTime}</div>
+          <div class="changed">Updated ${this.updatedTime}</div>
+        </div>
+      </div>
+    `;
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(siteCard.tag, siteCard);
